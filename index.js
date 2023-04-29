7497d918ff64ab5e76b55b2392fe80408c7f08fe
tarikul-islam <tariikul12@gmail.com>
2023-04-29 08:29:41 +0600

    Ready for client site news,category news, single/id news

--- index.js
+++ index.js
@@ -3,14 +3,37 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const news = require('./News.json');
 
 app.use(cors())
+
 app.get('/',(req, res) =>{
     res.send('Dragon is Running')
 });
 app.get('/categories', (req, res) =>{
     res.send(categories)
+})
+app.get('/news', (req, res) =>{
+    res.send(news)
+})
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const selectedNews = news.find(n =>n._id ===id);
+    res.send(selectedNews)
+})
+app.get('/categories/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    if(id === 0){
+        res.send(news)
+    }
+    else{
+
+        const categoriesNews = news.filter(n =>parseInt(n.category_id) ===id);
+        res.send(categoriesNews)
+    }
 })
 
 app.listen(port, ()=>{
